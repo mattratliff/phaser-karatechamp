270112d48b7af2fcad7d1bf3c716b29de9062278
Matt Ratliff <matt.ratliff@ozmo-mac-2005.local>
2021-01-26 15:35:38 -0500

    Add hitting and blocking JSON for collision

---
+++ src/config/blocking.js
@@ -0,0 +1,17 @@
+[
+	{
+		label: middleblock,
+		hitframes: ["05", "06", "07"],
+		facingForward: true
+	},
+		{
+		label: lowblock,
+		hitframes: ["05", "06", "07"],
+		facingForward: true
+	},
+		{
+		label: highblock,
+		hitframes: ["03", "04", "05", "06", "07", "08"],
+		facingForward: true
+	}
+]
